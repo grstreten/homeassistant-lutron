79bc05ea2d86b88c7a8a5263e3cbcd3970625853
George Streten <grstreten@me.com>
2023-09-22 12:34:19 +0100

    integrated tilt functionality (explorative)

--- custom_components/lutron_custom/cover.py
+++ custom_components/lutron_custom/cover.py
@@ -6,6 +6,7 @@ from typing import Any
 
 from homeassistant.components.cover import (
     ATTR_POSITION,
+    ATTR_TILT_POSITION,
     CoverEntity,
     CoverEntityFeature,
 )
@@ -30,27 +31,61 @@ def setup_platform(
         dev = LutronCover(area_name, device, hass.data[LUTRON_CONTROLLER])
         devs.append(dev)
 
+    for area_name, device in hass.data[LUTRON_DEVICES]["venetian"]:
+        dev = LutronCover(area_name, device, hass.data[LUTRON_CONTROLLER], tilt=True)
+        devs.append(dev)
+
     add_entities(devs, True)
 
 
 class LutronCover(LutronDevice, CoverEntity):
     """Representation of a Lutron shade."""
 
-    _attr_supported_features = (
-        CoverEntityFeature.OPEN
-        | CoverEntityFeature.CLOSE
-        | CoverEntityFeature.SET_POSITION
-    )
+    # Set up the Lutron Cover - allowing tilt if it's a venetian blind.
+    def __init__(
+        self,
+        area_name: str,
+        lutron_device: LutronDevice,
+        tilt: bool = False,
+    ) -> None:
+        """Initialize the Lutron cover."""
+        super().__init__(area_name, lutron_device)
+        self._tilt = tilt
+        if self._tilt:
+            self._attr_supported_features = (
+                CoverEntityFeature.OPEN
+                | CoverEntityFeature.CLOSE
+                | CoverEntityFeature.SET_POSITION
+                | CoverEntityFeature.OPEN_TILT
+                | CoverEntityFeature.CLOSE_TILT
+                | CoverEntityFeature.SET_TILT_POSITION
+            )
+        else:
+            self._attr_supported_features = (
+                CoverEntityFeature.OPEN
+                | CoverEntityFeature.CLOSE
+                | CoverEntityFeature.SET_POSITION
+            )
 
     @property
     def is_closed(self) -> bool:
         """Return if the cover is closed."""
         return self._lutron_device.last_level() < 1
+    
+    @property
+    def is_tilt_closed(self) -> bool:
+        """Return if the cover is closed."""
+        return self._lutron_device.last_tilt() < 1
 
     @property
     def current_cover_position(self) -> int:
         """Return the current position of cover."""
         return self._lutron_device.last_level()
+    
+    @property
+    def current_cover_tilt_position(self) -> int:
+        """Return the current position of cover."""
+        return self._lutron_device.last_tilt()
 
     def close_cover(self, **kwargs: Any) -> None:
         """Close the cover."""
@@ -66,11 +101,17 @@ class LutronCover(LutronDevice, CoverEntity):
             position = kwargs[ATTR_POSITION]
             self._lutron_device.level = position
 
+    def set_cover_tilt_position(self, **kwargs: Any) -> None:
+        """Move the shade to a specific position."""
+        if ATTR_TILT_POSITION in kwargs:
+            tilt_position = kwargs[ATTR_TILT_POSITION]
+            self._lutron_device.tilt = tilt_position
+
     def update(self) -> None:
         """Call when forcing a refresh of the device."""
         # Reading the property (rather than last_level()) fetches value
         level = self._lutron_device.level
-        _LOGGER.debug("Lutron ID: %d updated to %f", self._lutron_device.id, level)
+        _LOGGER.debug("Lutron ID: %d updated to %f position", self._lutron_device.id, level)
 
     @property
     def extra_state_attributes(self) -> dict[str, Any]:
